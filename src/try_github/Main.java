deddc5fd68b047185e4f1732db80c2eb70cbf474
SPL-023 <SPL-023@SPL-023-PC>
2016-05-06 17:32:48 +0900

    ついか

--- src/try_github/Main.java
+++ src/try_github/Main.java
@@ -5,7 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello Git with Eclipse");
-
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
+		System.out.println("Hello Git with Eclipse");
 	}
 
 }
